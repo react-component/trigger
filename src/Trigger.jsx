101da5555209bc1c29187d808f21ecaea6e1c8e6
Wei Zhu <yesmeck@gmail.com>
2017-05-17 10:51:50 +0800

    fix: this.popupInstance should be this._component (#53)
    After 5337b18, this.popupInstance had renamed to this._component

--- src/Trigger.jsx
+++ src/Trigger.jsx
@@ -430,8 +430,8 @@ const Trigger = createReactClass({
     return action.indexOf('focus') !== -1 || hideAction.indexOf('blur') !== -1;
   },
   forcePopupAlign() {
-    if (this.state.popupVisible && this.popupInstance && this.popupInstance.alignInstance) {
-      this.popupInstance.alignInstance.forceAlign();
+    if (this.state.popupVisible && this._component && this._component.alignInstance) {
+      this._component.alignInstance.forceAlign();
     }
   },
 
