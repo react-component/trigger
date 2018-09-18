39e1ccded36d188b2020190375f56c7f5f9aa51b
zombiej <smith3816@gmail.com>
2018-09-18 16:50:24 +0800

    popupInner should be class to use ref

--- src/PopupInner.js
+++ src/PopupInner.js
@@ -1,36 +1,40 @@
-import React from 'react';
+/* eslint react/prefer-stateless-function:0 */
+
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import LazyRenderBox from './LazyRenderBox';
 
-const PopupInner = (props) => {
-  const {
-    prefixCls, visible, style, className,
-    onMouseEnter, onMouseLeave,
-    children,
-  } = props;
+class PopupInner extends Component {
+  static propTypes = {
+    visible: PropTypes.bool,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    prefixCls: PropTypes.string,
+    onMouseEnter: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+    children: PropTypes.node,
+  };
 
-  return (
-    <div
-      className={className}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      style={style}
-    >
-      <LazyRenderBox className={`${prefixCls}-content`} visible={visible}>
-        {children}
-      </LazyRenderBox>
-    </div>
-  );
-};
-
-PopupInner.propTypes = {
-  visible: PropTypes.bool,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  prefixCls: PropTypes.string,
-  onMouseEnter: PropTypes.func,
-  onMouseLeave: PropTypes.func,
-  children: PropTypes.node,
-};
+  render() {
+    const {
+      prefixCls, visible, style, className,
+      onMouseEnter, onMouseLeave,
+      children,
+    } = this.props;
+  
+    return (
+      <div
+        className={className}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        style={style}
+      >
+        <LazyRenderBox className={`${prefixCls}-content`} visible={visible}>
+          {children}
+        </LazyRenderBox>
+      </div>
+    );
+  }
+}
 
 export default PopupInner;
