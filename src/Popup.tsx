4ac2f63c18794dd24612ceea6443ec761346d533
zombiej <smith3816@gmail.com>
2019-10-09 17:15:09 +0800

    add comment

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -20,6 +20,8 @@ import {
  * Popup should follow the steps for each component work correctly:
  * measure - check for the current stretch size
  * align - let component align the position
+ * afterAlign - choice next step is trigger motion or finished
+ * beforeMotion - should reset motion to invisible so that CSSMotion can do normal motion
  * motion - play the motion
  * stable - everything is done
  */
