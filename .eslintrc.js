9cddc0156bae91c141a469eb416f4cc719f313d0
zombiej <smith3816@gmail.com>
2019-10-10 16:03:51 +0800

    disable sort-comp

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'react/no-find-dom-node': 0,
     'react/no-did-update-set-state': 0,
     'react/no-unused-state': 1,
+    'react/sort-comp': 0,
     'jsx-a11y/label-has-for': 0,
     'jsx-a11y/label-has-associated-control': 0,
   },
