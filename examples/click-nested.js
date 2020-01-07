438e03b3c71ce14e4d5ed2a0dc5817a5803364ed
zombiej <smith3816@gmail.com>
2020-01-07 17:45:06 +0800

    fix: Popup with stop mouseDown popup should not close popup

---
+++ examples/click-nested.js
@@ -0,0 +1,90 @@
+/* eslint no-console:0 */
+
+import React from 'react';
+import Trigger from '../src';
+import '../assets/index.less';
+
+const builtinPlacements = {
+  left: {
+    points: ['cr', 'cl'],
+  },
+  right: {
+    points: ['cl', 'cr'],
+  },
+  top: {
+    points: ['bc', 'tc'],
+  },
+  bottom: {
+    points: ['tc', 'bc'],
+  },
+  topLeft: {
+    points: ['bl', 'tl'],
+  },
+  topRight: {
+    points: ['br', 'tr'],
+  },
+  bottomRight: {
+    points: ['tr', 'br'],
+  },
+  bottomLeft: {
+    points: ['tl', 'bl'],
+  },
+};
+
+const popupBorderStyle = {
+  border: '1px solid red',
+  padding: 10,
+  background: 'rgba(255, 0, 0, 0.1)',
+};
+
+function saveRef(name, component) {
+  this[name] = component;
+}
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.saveContainerRef = saveRef.bind(this, 'containerInstance');
+  }
+
+  render() {
+    return (
+      <div style={{ margin: 200 }}>
+        <div>
+          <Trigger
+            popupPlacement="right"
+            action={['click']}
+            builtinPlacements={builtinPlacements}
+            popup={
+              // Level 2
+              <Trigger
+                popupPlacement="right"
+                action={['click']}
+                builtinPlacements={builtinPlacements}
+                popup={<div style={popupBorderStyle}>i am a click popup</div>}
+              >
+                <div style={popupBorderStyle}>
+                  i am a click popup{' '}
+                  <button
+                    type="button"
+                    onMouseDown={e => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                    }}
+                  >
+                    I am preventPop
+                  </button>
+                </div>
+              </Trigger>
+            }
+          >
+            <span>Click Me</span>
+          </Trigger>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Test;
