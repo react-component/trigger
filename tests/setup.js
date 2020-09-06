2d068b20bc7f2756f705e6ad5e5d9e6b62ebc825
zombiej <smith3816@gmail.com>
2020-09-06 14:33:39 +0800

    add polyfill

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,6 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
+require('regenerator-runtime/runtime');
 
 window.requestAnimationFrame = func => {
   window.setTimeout(func, 16);
