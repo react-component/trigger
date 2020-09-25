7d222196ff500b8796cadfc377fa5ed92b8f15f7
二货机器人 <smith3816@gmail.com>
2020-09-25 14:20:01 +0800

    fix: Patch for getPopupContainer check (#200)
    * chore: Not trigger getContainer when dom not ready
    
    * test: check for nodes test
    
    * update deps
    
    * clean up

--- src/index.tsx
+++ src/index.tsx
@@ -146,6 +146,8 @@ export function generateTrigger(
 
     triggerRef = React.createRef<React.ReactInstance>();
 
+    attachId?: number;
+
     clickOutsideHandler: CommonEventHandler;
 
     touchOutsideHandler: CommonEventHandler;
@@ -540,16 +542,26 @@ export function generateTrigger(
     };
 
     attachParent = (popupContainer: HTMLDivElement) => {
-      const { props } = this;
-      const mountNode = props.getPopupContainer
-        ? props.getPopupContainer(this.getRootDomNode())
-        : props.getDocument().body;
+      raf.cancel(this.attachId);
+
+      const { getPopupContainer, getDocument } = this.props;
+      const domNode = this.getRootDomNode();
+
+      let mountNode: HTMLElement;
+      if (!getPopupContainer) {
+        mountNode = getDocument().body;
+      } else if (domNode || getPopupContainer.length === 0) {
+        // Compatible for legacy getPopupContainer with domNode argument.
+        // If no need `domNode` argument, will call directly.
+        // https://codesandbox.io/s/eloquent-mclean-ss93m?file=/src/App.js
+        mountNode = getPopupContainer(domNode);
+      }
 
       if (mountNode) {
         mountNode.appendChild(popupContainer);
       } else {
         // Retry after frame render in case parent not ready
-        raf(() => {
+        this.attachId = raf(() => {
           this.attachParent(popupContainer);
         });
       }
