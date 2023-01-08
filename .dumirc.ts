c3819612673bb0fc875de0ca5ed51858fe783dfc
kiner-tang(文辉) <1127031143@qq.com>
2023-01-08 10:33:52 +0800

    chore: update dumi config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'dumi';
+import path from 'path';
 
 export default defineConfig({
+  alias: {
+    'rc-trigger$': path.resolve('src'),
+    'rc-trigger/es': path.resolve('src'),
+  },
+  favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
     name: 'Trigger',
+    logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
 });
