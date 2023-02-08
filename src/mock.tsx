7ca188129983d79085cbe5d75b5b8551a68d9e87
二货机器人 <smith3816@gmail.com>
2023-02-08 11:52:03 +0800

    chore: fix portal mock

--- src/mock.tsx
+++ src/mock.tsx
@@ -2,19 +2,33 @@ import * as React from 'react';
 import { generateTrigger } from './index';
 
 interface MockPortalProps {
+  open?: boolean;
+  autoDestroy?: boolean;
   children: React.ReactElement;
   getContainer?: () => HTMLElement;
 }
 
 const MockPortal: React.FC<MockPortalProps> = ({
+  open,
+  autoDestroy,
   children,
   getContainer,
 }) => {
+  const [visible, setVisible] = React.useState(open);
+
   React.useEffect(() => {
     getContainer?.();
   });
 
-  return children;
+  React.useEffect(() => {
+    if (open) {
+      setVisible(true);
+    } else if (!open && autoDestroy) {
+      setVisible(false);
+    }
+  }, [open, autoDestroy]);
+
+  return visible ? children : null;
 };
 
 export default generateTrigger(MockPortal);
