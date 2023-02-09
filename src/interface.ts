d83c10a4b80233ba0783ff4b0f2e4cefb16b620a
二货机器人 <smith3816@gmail.com>
2023-02-09 11:11:15 +0800

    feat: add autoArrow

--- src/interface.ts
+++ src/interface.ts
@@ -14,68 +14,6 @@ export type Placement =
   | 'rightTop'
   | 'rightBottom';
 
-export interface TriggerProps {
-  children: React.ReactElement;
-  action?: ActionType | ActionType[];
-  showAction?: ActionType[];
-  hideAction?: ActionType[];
-  getPopupClassNameFromAlign?: (align: AlignType) => string;
-  onPopupVisibleChange?: (visible: boolean) => void;
-  onPopupClick?: React.MouseEventHandler<HTMLDivElement>;
-  afterPopupVisibleChange?: (visible: boolean) => void;
-  popup: React.ReactNode | (() => React.ReactNode);
-  popupStyle?: React.CSSProperties;
-  prefixCls?: string;
-  popupClassName?: string;
-  className?: string;
-  popupPlacement?: string;
-  builtinPlacements?: BuildInPlacements;
-  mouseEnterDelay?: number;
-  mouseLeaveDelay?: number;
-  zIndex?: number;
-  focusDelay?: number;
-  blurDelay?: number;
-  getPopupContainer?: (node: HTMLElement) => HTMLElement;
-  getDocument?: (element?: HTMLElement) => HTMLDocument;
-  forceRender?: boolean;
-  destroyPopupOnHide?: boolean;
-  mask?: boolean;
-  maskClosable?: boolean;
-  onPopupAlign?: (element: HTMLElement, align: AlignType) => void;
-  popupAlign?: AlignType;
-  popupVisible?: boolean;
-  defaultPopupVisible?: boolean;
-  autoDestroy?: boolean;
-
-  stretch?: string;
-  alignPoint?: boolean; // Maybe we can support user pass position in the future
-
-  /** Set popup motion. You can ref `rc-motion` for more info. */
-  popupMotion?: CSSMotionProps;
-  /** Set mask motion. You can ref `rc-motion` for more info. */
-  maskMotion?: CSSMotionProps;
-
-  /** @deprecated Please us `popupMotion` instead. */
-  popupTransitionName?: TransitionNameType;
-  /** @deprecated Please us `popupMotion` instead. */
-  popupAnimation?: AnimationType;
-  /** @deprecated Please us `maskMotion` instead. */
-  maskTransitionName?: TransitionNameType;
-  /** @deprecated Please us `maskMotion` instead. */
-  maskAnimation?: string;
-
-  /**
-   * @private Get trigger DOM node.
-   * Used for some component is function component which can not access by `findDOMNode`
-   */
-  getTriggerDOMNode?: (node: React.ReactInstance) => HTMLElement;
-
-  // ========================== Mobile ==========================
-  /** @private Bump fixed position at bottom in mobile.
-   * This is internal usage currently, do not use in your prod */
-  mobile?: MobileConfig;
-}
-
 export type AlignPointTopBottom = 't' | 'b' | 'c';
 export type AlignPointLeftRight = 'l' | 'r' | 'c';
 
@@ -108,6 +46,8 @@ export interface AlignType {
     shiftX?: boolean | number;
     shiftY?: boolean | number;
   };
+  /** Auto adjust arrow position */
+  autoArrow?: boolean;
   /**
    * Whether use css right instead of left to position
    */
