44fbfe842476f3d682d7cb53705f6d7bc628881e
二货机器人 <smith3816@gmail.com>
2023-03-17 00:04:05 +0800

    chore: clean up

--- tests/mobile.test.tsx
+++ tests/mobile.test.tsx
@@ -1,5 +1,6 @@
 import { act, fireEvent, render } from '@testing-library/react';
 import isMobile from 'rc-util/lib/isMobile';
+import React from 'react';
 import Trigger from '../src';
 import { placementAlignMap } from './util';
 
@@ -49,89 +50,86 @@ describe('Trigger.Mobile', () => {
     expect(document.querySelector('.rc-trigger-popup')).toBeTruthy();
   });
 
-  // function getTrigger(
-  //   props?: Partial<
-  //     TriggerProps & React.ClassAttributes<InstanceType<typeof Trigger>>
-  //   >,
-  // ) {
-  //   return (
-  //     <Trigger
-  //       action={['click']}
-  //       popupAlign={placementAlignMap.left}
-  //       popup={<strong className="x-content" />}
-  //       mask
-  //       maskClosable
-  //       mobile={{}}
-  //       {...props}
-  //     >
-  //       <div className="target">click</div>
-  //     </Trigger>
-  //   );
-  // }
+  // ZombieJ: back when we plan to support mobile
+  function getTrigger(props?: any) {
+    return (
+      <Trigger
+        action={['click']}
+        popupAlign={placementAlignMap.left}
+        popup={<strong className="x-content" />}
+        mask
+        maskClosable
+        mobile={{}}
+        {...props}
+      >
+        <div className="target">click</div>
+      </Trigger>
+    );
+  }
 
-  // it('mobile config', () => {
-  //   const { container } = render(
-  //     getTrigger({
-  //       mobile: {
-  //         popupClassName: 'mobile-popup',
-  //         popupStyle: { background: 'red' },
-  //       },
-  //     }),
-  //   );
+  it('mobile config', () => {
+    const { container } = render(
+      getTrigger({
+        mobile: {
+          popupClassName: 'mobile-popup',
+          popupStyle: { background: 'red' },
+        },
+      }),
+    );
 
-  //   fireEvent.click(container.querySelector('.target'));
+    fireEvent.click(container.querySelector('.target'));
 
-  //   expect(document.querySelector('.rc-trigger-popup')).toHaveClass(
-  //     'mobile-popup',
-  //   );
+    expect(document.querySelector('.rc-trigger-popup')).toHaveClass(
+      'mobile-popup',
+    );
 
-  //   expect(document.querySelector('.rc-trigger-popup')).toHaveStyle({
-  //     background: 'red',
-  //   });
-  // });
+    expect(document.querySelector('.rc-trigger-popup')).toHaveStyle({
+      background: 'red',
+    });
+  });
 
-  // it('popupRender', () => {
-  //   const { container } = render(
-  //     getTrigger({
-  //       mobile: {
-  //         popupRender: (node) => (
-  //           <>
-  //             <div>Light</div>
-  //             {node}
-  //           </>
-  //         ),
-  //       },
-  //     }),
-  //   );
+  it('popupRender', () => {
+    const { container } = render(
+      getTrigger({
+        mobile: {
+          popupRender: (node) => (
+            <>
+              <div>Light</div>
+              {node}
+            </>
+          ),
+        },
+      }),
+    );
 
-  //   fireEvent.click(container.querySelector('.target'));
-  //   expect(document.querySelector('.rc-trigger-popup')).toMatchSnapshot();
-  // });
+    fireEvent.click(container.querySelector('.target'));
+    expect(document.querySelector('.rc-trigger-popup')).toMatchSnapshot();
+  });
 
-  // it('click inside not close', () => {
-  //   const triggerRef = createRef<InstanceType<typeof Trigger>>();
-  //   const { container } = render(getTrigger({ ref: triggerRef }));
-  //   fireEvent.click(container.querySelector('.target'));
-  //   expect(triggerRef.current.state.popupVisible).toBeTruthy();
-  //   fireEvent.click(document.querySelector('.x-content'));
-  //   expect(triggerRef.current.state.popupVisible).toBeTruthy();
+  it('click inside not close', () => {
+    const triggerRef = React.createRef<any>();
+    const { container } = render(getTrigger({ ref: triggerRef }));
+    fireEvent.click(container.querySelector('.target'));
+    expect(triggerRef.current.state.popupVisible).toBeTruthy();
+    fireEvent.click(document.querySelector('.x-content'));
+    expect(triggerRef.current.state.popupVisible).toBeTruthy();
 
-  //   // Document click
-  //   act(() => {
-  //     fireEvent.mouseDown(document);
-  //   });
-  //   expect(triggerRef.current.state.popupVisible).toBeFalsy();
-  // });
+    // Document click
+    act(() => {
+      fireEvent.mouseDown(document);
+    });
+    expect(triggerRef.current.state.popupVisible).toBeFalsy();
+  });
 
-  // it('legacy array children', () => {
-  //   const { container } = render(
-  //     getTrigger({
-  //       popup: [<div key={0}>Light</div>, <div key={1}>Bamboo</div>],
-  //     }),
-  //   );
-  //   fireEvent.click(container.querySelector('.target'));
-  //   expect(document.querySelectorAll('.rc-trigger-popup-content')).toHaveLength(
-  //     1,
-  //   );
-  // });
+  it('legacy array children', () => {
+    const { container } = render(
+      getTrigger({
+        popup: [<div key={0}>Light</div>, <div key={1}>Bamboo</div>],
+      }),
+    );
+    fireEvent.click(container.querySelector('.target'));
+    expect(document.querySelectorAll('.rc-trigger-popup-content')).toHaveLength(
+      1,
+    );
+  });
 });
