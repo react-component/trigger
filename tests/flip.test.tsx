cca3e3bef63abd2cf87f7e7e515baabe1abc2139
二货机器人 <smith3816@gmail.com>
2023-04-10 17:08:04 +0800

    test: add test case

--- tests/flip.test.tsx
+++ tests/flip.test.tsx
@@ -1,7 +1,15 @@
 import { act, cleanup, render } from '@testing-library/react';
+import { _rs } from 'rc-resize-observer';
 import { spyElementPrototypes } from 'rc-util/lib/test/domHook';
+import type { TriggerProps } from '../src';
 import Trigger from '../src';
 import { getVisibleArea } from '../src/util';
+
+const flush = async () => {
+  await act(async () => {
+    await Promise.resolve();
+  });
+};
 
 const builtinPlacements = {
   top: {
@@ -139,9 +147,7 @@ describe('Trigger.Align', () => {
           </Trigger>,
         );
 
-        await act(async () => {
-          await Promise.resolve();
-        });
+        await flush();
 
         expect(document.querySelector(className)).toBeTruthy();
       });
@@ -188,9 +194,7 @@ describe('Trigger.Align', () => {
           </Trigger>,
         );
 
-        await act(async () => {
-          await Promise.resolve();
-        });
+        await flush();
 
         expect(document.querySelector(className)).toBeTruthy();
       });
@@ -257,9 +261,7 @@ describe('Trigger.Align', () => {
       </Trigger>,
     );
 
-    await act(async () => {
-      await Promise.resolve();
-    });
+    await flush();
 
     // Flip
     expect(
@@ -420,12 +422,134 @@ describe('Trigger.Align', () => {
       </Trigger>,
     );
 
-    await act(async () => {
-      await Promise.resolve();
-    });
+    await flush();
 
     expect(
       document.querySelector('.rc-trigger-popup-placement-bottom'),
     ).toBeTruthy();
+  });
+
+  // https://github.com/ant-design/ant-design/issues/41728
+  describe('save prev flip position', () => {
+    const flipList: {
+      name: string;
+      placement: string;
+      x?: number;
+      y?: number;
+      className: string;
+
+      // Move target position should back to origin placement which is visible
+      backX?: number;
+      backY?: number;
+      backClassName: string;
+    }[] = [
+      {
+        name: 'top2bottom',
+        placement: 'top',
+        y: 20,
+        className: '.rc-trigger-popup-placement-bottom',
+        backY: 95,
+        backClassName: '.rc-trigger-popup-placement-top',
+      },
+      {
+        name: 'bottom2top',
+        placement: 'bottom',
+        y: 80,
+        className: '.rc-trigger-popup-placement-top',
+        backY: 5,
+        backClassName: '.rc-trigger-popup-placement-bottom',
+      },
+      {
+        name: 'left2right',
+        placement: 'left',
+        x: 20,
+        className: '.rc-trigger-popup-placement-right',
+        backX: 95,
+        backClassName: '.rc-trigger-popup-placement-left',
+      },
+      {
+        name: 'right2left',
+        placement: 'right',
+        x: 80,
+        className: '.rc-trigger-popup-placement-left',
+        backX: 5,
+        backClassName: '.rc-trigger-popup-placement-right',
+      },
+    ];
+
+    flipList.forEach(
+      ({
+        name,
+        placement,
+        x = 0,
+        y = 0,
+        backX = 0,
+        backY = 0,
+        className,
+        backClassName,
+      }) => {
+        it(name, async () => {
+          spanRect.x = x;
+          spanRect.y = y;
+          popupRect.width = 30;
+          popupRect.height = 30;
+
+          const onPopupAlign = jest.fn();
+
+          const Demo = ({ popupPlacement }: Partial<TriggerProps>) => (
+            <Trigger
+              popupVisible
+              popupPlacement={popupPlacement}
+              builtinPlacements={builtinPlacements}
+              popup={<strong>trigger</strong>}
+              onPopupAlign={onPopupAlign}
+            >
+              <span className="target" />
+            </Trigger>
+          );
+
+          render(<Demo popupPlacement={placement} />);
+
+          await flush();
+
+          expect(document.querySelector(className)).toBeTruthy();
+          expect(onPopupAlign).toHaveBeenCalled();
+          onPopupAlign.mockReset();
+
+          // Change size to small than target position
+          popupRect.width = 10;
+          popupRect.height = 10;
+
+          act(() => {
+            _rs([
+              {
+                target: document.querySelector('.rc-trigger-popup'),
+              } as ResizeObserverEntry,
+            ]);
+          });
+          await flush();
+
+          expect(document.querySelector(className)).toBeTruthy();
+          expect(onPopupAlign).toHaveBeenCalled();
+          onPopupAlign.mockReset();
+
+          // Change target position to back of origin placement
+          spanRect.x = backX;
+          spanRect.y = backY;
+
+          act(() => {
+            _rs([
+              {
+                target: document.querySelector('.target'),
+              } as ResizeObserverEntry,
+            ]);
+          });
+          await flush();
+
+          expect(document.querySelector(backClassName)).toBeTruthy();
+          expect(onPopupAlign).toHaveBeenCalled();
+        });
+      },
+    );
   });
 });
