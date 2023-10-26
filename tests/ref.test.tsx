f3769269d253445c10032249baf836162d1badf7
二货机器人 <smith3816@gmail.com>
2023-10-26 17:36:22 +0800

    feat: trigger support nativeElement

---
+++ tests/ref.test.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable max-classes-per-file */
+
+import { cleanup, render } from '@testing-library/react';
+import { spyElementPrototypes } from 'rc-util/lib/test/domHook';
+import React from 'react';
+import Trigger, { type TriggerRef } from '../src';
+
+describe('Trigger.Ref', () => {
+  beforeAll(() => {
+    spyElementPrototypes(HTMLElement, {
+      offsetParent: {
+        get: () => document.body,
+      },
+    });
+  });
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    cleanup();
+    jest.useRealTimers();
+  });
+
+  it('support nativeElement', () => {
+    const triggerRef = React.createRef<TriggerRef>();
+
+    const { container } = render(
+      <Trigger ref={triggerRef} popup={<div />}>
+        <button />
+      </Trigger>,
+    );
+
+    expect(triggerRef.current.nativeElement).toBe(
+      container.querySelector('button'),
+    );
+  });
+});
